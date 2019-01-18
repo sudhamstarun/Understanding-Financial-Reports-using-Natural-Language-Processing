d7ed8774104eeff394c506e8ccbf6c29f14eb81e
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-01-18 14:26:42 +0530

    stop words remover added

--- Bash Scripts and Python Scripts/HTML_Parser.py
+++ Bash Scripts and Python Scripts/HTML_Parser.py
@@ -1,6 +1,12 @@
 import simplejson
 import sys
+import nltk
+import sys 
+import string
+
 from HTMLParser import HTMLParser
+from nltk.corpus import stopwords
+from nltk.tokenize import sent_tokenize, word_tokenize
 
 program_name = sys.argv[0]
 arguments = sys.argv[1:]
@@ -27,16 +33,39 @@ f = open(arguments[0], 'r')
 text = f.read()
 f.close()
 parsed_output = strip_tags(text)
+text = ' '.join(parsed_output.split())
 
-f = open(arguments[0], 'w')
-f.write(parsed_output)
-f.close()
-
-with open(arguments[0]) as f:
-   text = ''.join(line.strip() for line in f)
-
+#write the parsed output to the same document
 f = open(arguments[0], 'w')
 f.write(text)
 f.close()
+
+#open same file to remove stop words
+f = open(arguments[0], 'r')
+text = f.read()
+
+tokenized = word_tokenize(text)
+stop_words = set(stopwords.words('english'))
+
+#tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
+#tfs = tfidf.fit_transform(token_dict.values())
+
+filtered_sentence = [w for w in tokenized if not w in stop_words] 
+filtered_sentence = [] 
+  
+for w in tokenized: 
+    if w not in stop_words: 
+        filtered_sentence.append(w) 
+  
+#print(tokenized) 
+#print(filtered_sentence) 
+
+final_text = ' '.join(filtered_sentence)
+
+f = open('outputs.txt', 'w')
+f.write(final_text)
+f.close()
+
+
 
 
