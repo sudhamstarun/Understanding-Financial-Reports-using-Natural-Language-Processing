af75d56d61225c1bf50d8390c58a1c62e405bb89
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-01-27 19:38:21 +0800

    add files to gitignore

--- Bash Scripts and Python Scripts/Parse_StopWords.py
+++ Bash Scripts and Python Scripts/Parse_StopWords.py
@@ -1,44 +1,17 @@
-import nltk
-import sys 
-import string
-
-#from sklearn.feature_extraction.text import TfidfVectorizer
-from nltk.corpus import stopwords
-from nltk.tokenize import sent_tokenize, word_tokenize
-
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    stems = []
-    for item in tokens:
-        stems.append(PorterStemmer().stem(item))
-    return stems
-
-program_name = sys.argv[0]
-arguments = sys.argv[1:]
-count = len(arguments)
-
-#read file
-f = open(arguments[0], 'r')
-text = f.read()
-
-tokenized = word_tokenize(text)
-stop_words = set(stopwords.words('english'))
-
-#tfidf = TfidfVectorizer(tokenizer=tokenize, stop_words='english')
-#tfs = tfidf.fit_transform(token_dict.values())
-
-filtered_sentence = [w for w in tokenized if not w in stop_words] 
-filtered_sentence = [] 
-  
-for w in tokenized: 
-    if w not in stop_words: 
-        filtered_sentence.append(w) 
-  
-#print(tokenized) 
-#print(filtered_sentence) 
-
-final_text = ' '.join(filtered_sentence)
-
-f = open('outputs.txt', 'w')
-f.write(final_text)
-f.close()
+autoParse <- function(x){
+  paste(c(input, as.character(masterIndex$EDGAR_LINK[x])), collapse = "") %>% # Step 1
+  readLines(encoding = "UTF-8") %>% # Step 2
+  str_c(collapse = " ") %>% # Step 3
+  str_extract(pattern = "(?s)(?m)<TYPE>10-K.*?(</TEXT>)") %>% # Step 4
+  str_replace(pattern = "((?i)<TYPE>).*?(?=<)", replacement = "") %>% # Step 5
+  str_replace(pattern = "((?i)<SEQUENCE>).*?(?=<)", replacement = "") %>% # Step 6
+  str_replace(pattern = "((?i)<FILENAME>).*?(?=<)", replacement = "") %>%
+  str_replace(pattern = "((?i)<DESCRIPTION>).*?(?=<)", replacement = "") %>%
+  str_replace(pattern = "(?s)(?i)<head>.*?</head>", replacement = "") %>%
+  str_replace(pattern = "(?s)(?i)<(table).*?(</table>)", replacement = "") %>%
+  str_replace_all(pattern = "(?s)(?i)(?m)> +Item|>Item|^Item", replacement = ">°Item") %>% # Step 7
+  str_replace_all(pattern = "(?s)<.*?>", replacement = " ") %>% # Step 8
+  str_replace_all(pattern = "&(.{2,6});", replacement = " ") %>% # Step 9
+  str_replace_all(pattern = "(?s) +", replacement = " ") %>% # Step 10
+  write(file = paste(output, x, ".txt"), sep = "") # Step 11
+}
