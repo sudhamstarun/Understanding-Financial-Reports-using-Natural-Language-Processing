4accfdfe6c1d49dc22af5d5e9b39b4d22083a493
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-01-29 02:04:07 +0800

    added parsing stuff

--- Natural Language Processing/Word2Vec2GloVe.py
+++ Natural Language Processing/Word2Vec2GloVe.py
@@ -7,4 +7,8 @@ from gensim.models import KeyedVectors
 def changeFormat():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-w','--word2vec',type = str, required = True, help='Word2Vec Model')
+    args = parser.parse_args()
+
+    intermediate_storage = ".intermediate.txt"
+    changedFormatFileName = args.word2vec
     
