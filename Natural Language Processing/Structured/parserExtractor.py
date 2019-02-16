387ea7f9b74bee87159500a3d5618dc682c9c54e
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-02-17 01:18:35 +0800

    fix V4

--- Natural Language Processing/Structured/parserExtractor.py
+++ Natural Language Processing/Structured/parserExtractor.py
@@ -122,7 +122,8 @@ def get_tables(soup, p_counter, div_counter, table_counter):
         record = caption_tag.text
         name = "page_table" + str(counter)
         fname = name + ".txt"
-        mypath = "Page-Tag-Data/" + arguments[0].strip(".txt")
+        mypath = "/home2/vvsaripalli/SECReports/Page-Tag-Data/" +
+        arguments[0].strip(".txt") + "/"
 
         # Creating directory if it doesn't exist
         if not os.path.isdir(mypath):
