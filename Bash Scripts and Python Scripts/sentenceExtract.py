984aff6ca3c2c0bac6eb0fe83e619a058fdb857a
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-03-01 19:08:04 +0800

    VM testing 2

--- Bash Scripts and Python Scripts/sentenceExtract.py
+++ Bash Scripts and Python Scripts/sentenceExtract.py
@@ -1,18 +1,26 @@
-import sys
-import re
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+
+# reading and tokenizing the file
+f = open("test_data.json").read()
+sentences = sent_tokenize(f)
+
+# defining word list
+
+word_list = ["Notional Amount", "notional amount",
+             "reference entity", "expires", "due"]
 
 
-def find(infile, outfile):
-    with open(infile) as a, open(outfile, 'a') as b:
-        for line in a:
-            # matches = re.finditer(r"([^.]*?due([^\.]|\.(\d))*\.)",line, re.MULTILINE | re.IGNORECASE) )
-            # matches = re.finditer(r"( [^.]*?credit default swap([^\.]|\.(\d))*\.| [^.]*?counterparty([^\.]|\.(\d))*\.| [^.]*?receives([^\.]|\.(\d))*\.| [^.]*?buy([^\.]|\.(\d))*\.| [^.]*?pays([^\.]|\.(\d))*\.| [^.]*?receive([^\.]|\.(\d))*\.| [^.]*?notional amount([^\.]|\.(\d))*\.| [^.]*?due([^\.]|\.(\d))*\. )",line, re.MULTILINE | re.IGNORECASE) )
-            matches = re.finditer(r"([^.]*?receives([^\.]|\.(\d)|\.(\,))*\.| [^.]*?buy([^\.]|\.(\d)|\.(\,))*\.| [^.]*?sell([^\.]|\.(\d)|\.(\,))*\.|[^.]*?pays([^\.]|\.(\d)|\.(\,))*\.| [^.]*?\bpay\b([^\.]|\.(\d)|\.(\,))*\.|  [^.]*?reference entity([^\.]|\.(\d)|\.(\,))*\.| [^.]*?receive([^\.]|\.(\d)|\.(\,))*\.| [^.]*?expires([^\.]|\.(\d)|\.(\,))*\.|  [^.]*?fixed rate([^\.]|\.(\d)|\.(\,))*\.| [^.]*?quarterly([^\.]|\.(\d)|\.(\,))*\.|[^.]*?notional amount([^\.]|\.(\d)|\.(\,))*\.| [^.]*?due([^\.]|\.(\d)|\.(\,))*\. )", line, re.MULTILINE | re.IGNORECASE)
+def sentenceFinder(sentences, word_list):
+    for word in word_list:
+        my_sentence = [
+            sent for sent in sentences if word in word_tokenize(sent)]
 
-            for matchNum, match in enumerate(matches, start=1):
-
-                b.write(" {match}".format(
-                    matchNum=matchNum, start=match.start(), end=match.end(), match=match.group()))
+    return my_sentence
 
 
-find(sys.argv[1], sys.argv[2])
+sentences = sentenceFinder(sentences, word_list)
+
+# writing the output to the file
+with open("output.txt", "w") as f:
+    f.write(sentences)
