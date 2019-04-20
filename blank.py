4df7dd85959e94d82ad3cdc60fad7a97f099601f
Tarun Sudhams <tarun.sudhams1997@gmail.com>
2019-04-20 17:35:24 +0800

    update final reports

--- blank.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-filename = sys.argv[1]
-file = open(filename, "r")
-file_content = file.read()
-file.close()
-if file_content == "":
-    print("empty")
-
--- emptydir.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for dir in m_format/*/
-do
-
-if [ -n "$(find $dir -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
-	(rm -rf  $dir)
-fi
-done
-
